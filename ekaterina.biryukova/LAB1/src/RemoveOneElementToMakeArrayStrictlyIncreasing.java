1bdbb8d79473ec0a93a660f741809f84b47ef2d1
MrsSbr <daria.sanna@gmail.com>
2022-11-09 11:17:22 +0300

    Fixed 1 task

--- ekaterina.biryukova/LAB1/src/RemoveOneElementToMakeArrayStrictlyIncreasing.java
+++ ekaterina.biryukova/LAB1/src/RemoveOneElementToMakeArrayStrictlyIncreasing.java
@@ -18,15 +18,13 @@ public class RemoveOneElementToMakeArrayStrictlyIncreasing {
 
         for (int index = 0; index < size - 1; index++) {
             if (array[index] >= array[index + 1]) {
-                int i = index;
                 boolean isIncr = IsIncreasing(array, index + 1, size);
                 if (!isIncr) return false;
                 else {
-                    if (i == 0) return true;
-                    else if (array[i - 1] >= array[i + 1]) {
-                        if (array[i] < array[i + 2]) return true;
-                        else return false;
-                    } else if (array[i - 1] < array[i + 1])
+                    if (index == 0) return true;
+                    else if (array[index - 1] >= array[index + 1]) {
+                        return array[index] < array[index + 2];
+                    } else if (array[index - 1] < array[index + 1])
                         return true;
                 }
             }
