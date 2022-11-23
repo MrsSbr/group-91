e5b81a4b2818f2cc319b145d256429b8cfbfab1e
Tests000 <83328245+Tests000@users.noreply.github.com>
2022-11-23 08:49:31 +0300

    fix message

--- Арутюнян.Артур/lab1/src/App.java
+++ Арутюнян.Артур/lab1/src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Input count of commands");
+        System.out.println("Input number");
         try {
             int result = in.nextInt();
             System.out.println(Palindrom.checkPalindrom(result));
