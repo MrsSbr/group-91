de685c48139fe1f35b9b426d2e528523afa2af89
Tests000 <99970334+Artooorcheck@users.noreply.github.com>
2022-11-30 06:37:28 +0300

    fix warnings

--- Арутюнян.Артур/lab2/src/App.java
+++ Арутюнян.Артур/lab2/src/App.java
@@ -8,26 +8,25 @@ public class App {
     public static void main(String[] args) {
         var list = new ArrayList<InterfaceMedicine>();
         list.add(new Drug("Met", 2, 100, 1));
-        list.add(new Pilule("Pilule", 10, 1, FluidType.alcohol));
+        list.add(new Pilule("Pil", 10, 1, FluidType.alcohol));
         list.add(new Solute("sol", 5, 10));
         for(var iMed: list)
         {
-            System.out.println(iMed.toString());
-            if (iMed instanceof Solute) {
-                Solute solute = (Solute) iMed;
+            System.out.println(iMed);
+            if (iMed instanceof Solute solute) {
                 solute.drink(1);
-                System.out.println(solute.toString());
+                System.out.println(solute);
             }
         }
 
-        var pil1 = new Pilule("Pilule", 10, 1, FluidType.alcohol);
-        var pil2 = new Pilule("Pilule", 10, 1, FluidType.water);
+        var pil1 = new Pilule("Pil", 10, 1, FluidType.alcohol);
+        var pil2 = new Pilule("Pil", 10, 1, FluidType.water);
 
         for(var iMed: list)
         {
             if(iMed.equals(pil1))
             {
-                System.out.println(iMed.toString());
+                System.out.println(iMed);
                 System.out.println(iMed.equals(pil2));
             }
         }
