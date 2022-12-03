75450f4ddbf908499900afed80c2616b02a22c4f
Poliakova Polina <poliakova_polya@mail.ru>
2022-12-03 17:46:15 +0300

    Added 4th task

--- polina.poliakova/lab4/src/Main.java
+++ polina.poliakova/lab4/src/Main.java
@@ -1,5 +1,57 @@
+import modules.PlantStorage;
+import input.ConsoleInput;
+import input.InvalidBoundsException;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    private static final int END_MENU_ITEM = 4;
+
+    private static final String DATA_PATH = "polina.poliakova/lab3/group-91/polina.poliakova/lab4/data.txt";
+
+    public static void main(final String[] args) {
+        logger.log(Level.INFO, "Start program");
+
+        PlantStorage plantStorage = new PlantStorage();
+        plantStorage.fillFromFile(DATA_PATH);
+
+        try {
+            int selectedMenuItem;
+            do {
+                printMenu();
+                selectedMenuItem = ConsoleInput.inputIntInRange("Выберите пункт", 1, END_MENU_ITEM);
+                switch (selectedMenuItem) {
+                    case 1 -> System.out.println(plantStorage.getLessProfitablePlant());
+                    case 2 -> System.out.println(plantStorage.getAllSizesPotsForEveryPlant());
+                    case 3 -> System.out.println(plantStorage.getMonthsForYearWhenMostFloweringPlantsSold());
+                }
+                waitUser();
+            } while (selectedMenuItem != END_MENU_ITEM);
+        } catch (InvalidBoundsException e) {
+            logger.log(Level.SEVERE, "Input menu item exception", e);
+        }
+
+        logger.log(Level.OFF, "End program");
+    }
+
+    public static void printMenu() {
+        System.out.println("1: Найти растение(растения), на продаже которого(-ых) магазин заработал меньше всего");
+        System.out.println("2: Найти для каждого растения найти все размеры горшков, в которых оно продается");
+        System.out.println("3: Найти месяц, в котором было продано больше всего цветущих растений");
+        System.out.println("4: Выход");
+    }
+
+    public static void waitUser() {
+        System.out.println("Введите <ENTER>");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "wait press enter", e);
+        }
     }
 }
---
+++ polina.poliakova/lab4/src/modules/PlantStorage.java
@@ -0,0 +1,145 @@
+package modules;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PlantStorage {
+    private static final Logger logger = Logger.getLogger(PlantStorage.class.getName());
+    private final List<Plant> plants = new ArrayList<>();
+
+    public void fillFromFile(String filePath) {
+        logger.log(Level.INFO, "Start read file");
+
+        try (var bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Plant plant = Plant.parse(line);
+                if (plant == null) {
+                    logger.log(Level.SEVERE, "Parse plant error. Stop read");
+                    break;
+                }
+                plants.add(plant);
+            }
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "File not found", e);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Read file exception", e);
+        }
+
+        logger.log(Level.INFO, "End read file");
+    }
+
+    private List<Plant> getPlantsAfterDate(LocalDate date) {
+        List<Plant> plantsAfterDate = new ArrayList<>();
+        for (var plant : plants) {
+            if (plant.getDate().compareTo(date) >= 0) {
+                plantsAfterDate.add(plant);
+            }
+        }
+        return plantsAfterDate;
+    }
+
+    private void putMapEntryOnStringBuilder(StringBuilder stringBuilder, Map.Entry<String, Integer> entry) {
+        stringBuilder
+                .append(entry.getKey())
+                .append(" : ")
+                .append(entry.getValue())
+                .append("\n");
+    }
+
+    // - месяц, в котором было продано больше всего цветущих растений
+    private String getMaxValueString(Map<String, Integer> statictics) {
+        Integer maxAmount = -1;
+
+        for (var entry : statictics.entrySet()) {
+            if (entry.getValue() > maxAmount) {
+                maxAmount = entry.getValue();
+            }
+        }
+
+        StringBuilder maxAmountEntriesStringBuilder = new StringBuilder();
+
+        for (var entry : statictics.entrySet()) {
+            if (entry.getValue().equals(maxAmount)) {
+                putMapEntryOnStringBuilder(maxAmountEntriesStringBuilder, entry);
+            }
+        }
+
+        return maxAmountEntriesStringBuilder.toString();
+    }
+
+    // - месяц, в котором было продано больше всего цветущих растений
+    public String getMonthsForYearWhenMostFloweringPlantsSold() {
+        if (plants.isEmpty()) {
+            return "Нет растений :(";
+        }
+        Map<String, Integer> monthsSoldFloweringPlants = new HashMap<>();
+        var yearSoldPlants = getPlantsAfterDate(LocalDate.now().minusYears(1));
+
+        for (var plant : yearSoldPlants) {
+            String month = plant.getDate().getMonth().toString();
+            Integer floweringPlantAmount = monthsSoldFloweringPlants.get(month);
+            monthsSoldFloweringPlants.put(month, (plant.getTypeName() == PlantType.FLOWERING ? 1 : 0) + (floweringPlantAmount != null ? floweringPlantAmount : 0));
+        }
+
+        return getMaxValueString(monthsSoldFloweringPlants);
+    }
+
+    //- для каждого растения найти все размеры горшков, в которых оно продается
+    public String getAllSizesPotsForEveryPlant() {
+        if (plants.isEmpty()) {
+            return "Нет растений :(";
+        }
+
+        Map<String, Set<Double>> plantsWithSizePots = new HashMap<>();
+
+        for (var plant : plants) {
+            String plantName = plant.getPlantName();
+            Set<Double> potsSizes = plantsWithSizePots.get(plantName) == null ? new HashSet<>() : plantsWithSizePots.get(plantName);
+            potsSizes.add(plant.getSizePot());
+            plantsWithSizePots.put(plantName, potsSizes);
+        }
+
+        return mapStringBuilder(plantsWithSizePots);
+    }
+
+    //заменить в append!!!
+    public String mapStringBuilder(Map<String, Set<Double>> map) {
+        StringBuilder result = new StringBuilder();
+        //так лучше, чем += ?
+        for (var i : map.keySet()) {
+            result.append(i).append(" ").append(map.get(i).toString()).append("\n");
+        }
+
+        return result.toString();
+    }
+
+    //найти растение(растения), на продаже которого(-ых) магазин заработал меньше всего;
+    public String getLessProfitablePlant() {
+        if (plants.isEmpty()) {
+            return "Нет растений :(";
+        }
+        Map<String, Double> eachPlantAllIncome = new HashMap<>();
+        for (var plant : plants) {
+            String name = plant.getPlantName();
+            eachPlantAllIncome.put(name, eachPlantAllIncome.get(name) == null ? 0 : eachPlantAllIncome.get(name) + plant.getPrice());
+        }
+
+        //Map.Entry<String, Double> plantWithMinIncome = eachPlantAllIncome.entrySet().iterator().next();
+        String plantWithMinIncome = "";
+        Double minIncome = eachPlantAllIncome.entrySet().iterator().next().getValue();
+
+        for (var plant : eachPlantAllIncome.keySet()) {
+            if (eachPlantAllIncome.get(plant) < minIncome) {
+                minIncome = eachPlantAllIncome.get(plant);
+                plantWithMinIncome = plant;
+            }
+        }
+
+        return plantWithMinIncome;
+    }
+}
