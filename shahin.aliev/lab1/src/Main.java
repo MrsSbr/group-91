905f269f094263be5d53b98fb67f0fb0d54200d7
shahin_aliev <termisooo1@mail.ru>
2022-12-06 19:54:51 +0300

    add lab1 of aliev shahin

---
+++ shahin.aliev/lab1/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static int lastRemaining(int n) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            arr.add(i);
+        }
+        while (arr.size() != 1) {
+            int i = 0;
+            arr.remove(i);
+
+            while (arr.size() != 1 && i + 1 < arr.size()) {
+                i++;
+                arr.remove(i);
+            }
+
+            if (arr.size() != 1) {
+                i = arr.size() - 1;
+                arr.remove(i);
+                while (arr.size() != 1 && i - 2 >= 0) {
+                    i -= 2;
+                    arr.remove(i);
+                }
+            }
+        }
+        return arr.get(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите целое число:");
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.next());
+        System.out.println("Ответ: " + lastRemaining(num));
+    }
+}
