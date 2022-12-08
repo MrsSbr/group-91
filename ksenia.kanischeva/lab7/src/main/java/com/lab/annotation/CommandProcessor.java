6f7e5954ff0347bf0b8e33f06d21455208c1b12c
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2022-12-08 23:17:03 +0300

    to check processor

--- ksenia.kanischeva/lab7/src/main/java/com/lab/annotation/CommandProcessor.java
+++ ksenia.kanischeva/lab7/src/main/java/com/lab/annotation/CommandProcessor.java
@@ -6,10 +6,15 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.ExecutableType;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,22 +23,83 @@ import java.util.stream.Collectors;
 public class CommandProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        for (TypeElement annotation : annotations) {
-            Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
-            List<Element> incorrectMethods = annotatedElements.stream()
-                    .filter(element ->
-                            ((ExecutableType) element.asType()).getParameterTypes().size() != 1
-                                    || !((ExecutableType) element.asType()).getReturnType().toString().equals("String")
-                                    || !element.getSimpleName().toString().toLowerCase().contains("convert"))
-                    .collect(Collectors.toList());
 
-            incorrectMethods.forEach(element ->
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                            "Method" + element.getSimpleName().toString() + " is annotated as @StringCommand, " +
-                                    "but declaration is incorrect"));
+        Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(StringCommand.class);
+
+        Map<Boolean, List<Element>> annotatedMethods = annotatedElements.stream().collect(
+                Collectors.partitioningBy(element ->
+                        ((ExecutableType) element.asType()).getParameterTypes().size() == 1
+                                && ((ExecutableType) element.asType()).getReturnType().toString().equals("String")
+                                && !element.getModifiers().contains(Modifier.STATIC)));
+
+        List<Element> converters = annotatedMethods.get(true);
+        List<Element> otherMethods = annotatedMethods.get(false);
+
+        otherMethods.forEach(element ->
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                        "Method" + element.getSimpleName().toString() + " is annotated as @StringCommand, " +
+                                "but declaration is incorrect"));
+
+        if (converters.isEmpty()) {
+            return false;
         }
+//
+//        Map<Element, String> casesMap = converters.stream().collect(
+//                Collectors.toMap(c -> c, c -> c.getAnnotation(StringCommand.class).value()));
 
+
+        Map<Class<?>, List<Element>> classesMap = converters.stream()
+                .collect(Collectors.groupingBy(Element::getClass));
+
+        writeFiles(classesMap);
         return true;
+    }
+
+    private void writeFiles(Map<Class<?>, List<Element>> classesMap){
+        classesMap.forEach((cl, elems)-> {
+            try {
+                writeFile(cl, elems);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void writeFile(Class<?> classConverter, List<Element> methods) throws IOException {
+        String implClassName = classConverter.getSimpleName() + "Command";
+        String className = classConverter.getName();
+
+        JavaFileObject commandFile = processingEnv.getFiler()
+                .createSourceFile(className+ "Command");
+        try (PrintWriter out = new PrintWriter(commandFile.openWriter())) {
+            String classPackage = classConverter.getPackageName();
+
+            out.print("package ");
+            out.print(classPackage);
+            out.println(";");
+            out.println();
+
+            out.print("public class ");
+            out.print(implClassName + " extends " + classConverter.getSimpleName());
+            out.println(" {");
+            out.println();
+
+            for (var method : methods){
+                out.println("@Override");
+                out.print("    public String " + method.getSimpleName() + "(String str) {");
+                out.println("        return super."+ method.getSimpleName()+"(str);");
+//                switch (casesMap.get(method)){
+//                    case "camelCase"->
+//                    case "snake_case"-> out.println("        return str + 'S';");
+//                    case "kebab-case"-> out.println("        return str + 'K';");
+//                }
+                out.println("    }");
+                out.println();
+            }
+
+            out.println("}");
+
+        }
     }
 }
 
--- ksenia.kanischeva/lab7/src/main/java/com/lab/logic/StringConverter.java
+++ ksenia.kanischeva/lab7/src/main/java/com/lab/logic/StringConverter.java
@@ -5,22 +5,18 @@ import com.lab.annotation.StringCommand;
 public class StringConverter {
 
     @StringCommand("camelCase")
-    public static String convertToCamelCase(String str) {
+    public String convertToCamelCase(String str) {
         return "camelCase";
     }
 
     @StringCommand("snake_case")
-    public static String convertToSnakeCase(String str) {
+    public String convertToSnakeCase(String str) {
         return "snake_case";
     }
 
     @StringCommand("kebab-case")
-    public static String convertToKebabCase(String str) {
+    public String convertToKebabCase(String str) {
         return "kebab-case";
     }
 
-    @StringCommand("")
-    public static String incorrectMethod(String str) {
-        return "";
-    }
 }
