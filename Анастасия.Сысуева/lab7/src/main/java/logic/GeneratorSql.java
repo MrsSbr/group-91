fea003a2d044e22bb87595bdeca7ac0318d72c46
ak.sackamacki2016@yandex.ru <ak.sackamacki2016@yandex.ru>
2022-12-24 03:06:29 +0300

    Add 7 lab

---
+++ Анастасия.Сысуева/lab7/src/main/java/logic/GeneratorSql.java
@@ -0,0 +1,72 @@
+package logic;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class GeneratorSql {
+
+    public static String getTableName(Class<?> object) {
+        return object.getAnnotation(TableName.class).name();
+    }
+
+    public static String getFieldsName(Class<?> object) {
+        return Arrays.stream(object.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
+    }
+
+    public static String getField(Field field, Object values) {
+        field.setAccessible(true);
+        Object value;
+        try {
+            value = field.get(values);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        if (value.getClass() == String.class) {
+            return "'" + value + "'";
+        }
+        return value.toString();
+    }
+
+    public static String getFieldsValues(Object values) {
+        return Arrays.stream(values.getClass().getDeclaredFields())
+                .map(field -> getField(field, values))
+                .collect(Collectors.joining(", "));
+    }
+
+    public static String getSelectQuery(Class<?> object) {
+        return "SELECT " + getFieldsName(object) + " FROM " + getTableName(object);
+    }
+
+    public static String getInsertQuery(Object values) {
+        return "INSERT INTO " + getTableName(values.getClass()) + " VALUES (" + getFieldsValues(values) + ")";
+    }
+
+    public static String getSetString(Object values) {
+        return Arrays.stream(values.getClass().getDeclaredFields())
+                .filter(f -> !f.isAnnotationPresent(IdPK.class))
+                .map(f -> getEqualityString(f, values, f.getName()))
+                .collect(Collectors.joining(", "));
+    }
+
+    public static String getUpdateQuery(Object values) {
+        return "UPDATE " + getTableName(values.getClass()) + " SET " + getSetString(values) + " WHERE " + getCondition(values);
+    }
+
+    public static String getEqualityString(Field field, Object value, String name) {
+        return name + " = " + getField(field, value);
+    }
+
+    public static String getCondition(Object values) {
+        return Arrays.stream(values.getClass().getDeclaredFields())
+                .filter(p -> p.isAnnotationPresent(IdPK.class))
+                .map(f -> getEqualityString(f, values, f.getName()))
+                .collect(Collectors.joining(", "));
+    }
+
+    public static String getDeleteQuery(Object values) {
+        return "DELETE FROM " + getTableName(values.getClass()) + " WHERE " + getCondition(values);
+    }
+}
---
+++ Анастасия.Сысуева/lab7/src/test/java/data/AnimeTestsData.java
@@ -0,0 +1,12 @@
+package data;
+
+import entity.Anime;
+
+public class AnimeTestsData {
+    public static final Anime PROSE_OF_STRAY_DOGS_ANIME = new Anime(1, "Проза бродячих псов", "Такуя Игараси",
+            "детектив", 2016, 8.8);
+    public static final Anime NARUTO_ANIME = new Anime(2, "Наруто", "Хаято Датэ", "сёнэн",
+            2002, 9.2);
+    public static final Anime XORIMIYA_ANIME = new Anime(3, "Хоримия", "Масаси Исихама", "романтика",
+            2021, 9.4);
+}
