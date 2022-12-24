4a2c26093907ad623c485cacdd33254f7fdc8313
Test000 <Artooorcheck@gmail.com>
2022-12-24 09:52:39 +0300

    code formatting

--- Арутюнян.Артур/lab5/lab4/src/order/OrderStorage.java
+++ Арутюнян.Артур/lab5/lab4/src/order/OrderStorage.java
@@ -19,12 +19,11 @@ public class OrderStorage {
         logger.log(Level.INFO, "read_file_started");
 
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.forEach(line->{
+            stream.forEach(line -> {
                 Order order = Order.parse(line);
                 if (order == null) {
                     logger.log(Level.SEVERE, "order_cast_error");
-                }
-                else {
+                } else {
                     orders.add(order);
                 }
             });
@@ -47,25 +46,25 @@ public class OrderStorage {
         int restCount = getRestaurantsCount();
         return orders.stream()
                 .collect(Collectors.groupingBy(Order::courier, Collectors.groupingBy(Order::restaurantName)))
-                .entrySet().stream().filter(pair->pair.getValue().size()==restCount)
+                .entrySet().stream().filter(pair -> pair.getValue().size() == restCount)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toSet());
     }
 
     public String getMaxPopularRestForLastYear() {
         return orders.stream().filter(order -> order.startDate().getYear() == LocalDate.now().getYear())
-                .collect(Collectors.groupingBy(Order::restaurantName, Collectors.summingInt(order->order.composition().length)))
+                .collect(Collectors.groupingBy(Order::restaurantName, Collectors.summingInt(order -> order.composition().length)))
                 .entrySet()
                 .stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
     }
 
     public LocalDate getMaxSumTimeOrders() {
         return orders.stream().collect(Collectors.groupingBy(order -> {
-            int year = order.startDate().getYear();
-            int month = order.startDate().getMonthValue();
-            return LocalDate.parse(year + "-" + (month < 10 ? "0" + month : month) + "-01");
-        },
-                Collectors.summingInt(Order::deliveryTime))).entrySet()
+                            int year = order.startDate().getYear();
+                            int month = order.startDate().getMonthValue();
+                            return LocalDate.parse(year + "-" + (month < 10 ? "0" + month : month) + "-01");
+                        },
+                        Collectors.summingInt(Order::deliveryTime))).entrySet()
                 .stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
     }
 }
